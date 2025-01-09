069848f2845e3ce2c3f52a325ba46ae46a6b23fb
Christina Tsagkogianni <christina.tsagkogianni.2@ki.se>
2025-01-09 15:14:09 +0100

    package documentation

---
+++ R/package_documentation.R
@@ -0,0 +1,10 @@
+#' TurboID Analysis Package
+#'
+#' This package provides the functions needed to assign TurboID hits to either a
+#' true or false positive category, and to the create the ROC curves described 
+#' in Cho et al., 2020 (Nat Protoc).
+#'
+#' @docType package
+#' @name TurboIDanalysis
+#' @aliases TurboIDanalysis
+"_PACKAGE"
