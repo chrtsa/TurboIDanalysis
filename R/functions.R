f3407a42f201ed0947845a68fb6bcddd5cf3012f
Christina Tsagkogianni <christina.tsagkogianni.2@ki.se>
2025-01-10 15:28:13 +0100

    Debugging functions, finalising description and README, adding examples and citations

--- R/functions.R
+++ R/functions.R
@@ -1,25 +1,26 @@
-#' @title True Positive and False Positive for SYP
-#' @description This function calculates the True Positive Rate (TPR) and False Positive Rate (FPR) for the SYP dataset
-#' @param data The dataset that will be used. Ensure that the columns are named appropriately (gene_name for the proteins, and SYP_ratio for the ratio of interest)
+#' @title True Positive and False Positive Rates
+#' @description This function calculates the True Positive Rate (TPR) and False Positive Rate (FPR) for the provided dataset. First, the proteomics hits are flagged as TP or FP, based on the user-provided TP and FP lists, generated based on prior annotation and literature evidence. The proteins are then normalised against the distribution of FP proteins, and ranked in descending order. Then, the TPR and FPR are calculated at each potential cutoff. The optimal cutoff will be calculated based on the ROC at a later stage.
+#' @param data The dataset that will be used. 
 #' @param sample The name of the sample (e.g. SYP or NES) 
-#' @param TP The list of true positive proteins. Ensure that the column name is gene_name.
-#' @param FP The list of false positive proteins. Ensure that the column name is gene_name.
-#' @param ratio The column that contains the ratio of interest (SYP or NES)
-#' @param TP_flag The name of the column for the true positive flag (TP_SYP or TP_NES), so whether this protein is a TP for SYP or NES respectively
+#' @param TP A list or dataframe containing the true positive proteins, used for flagging. Contains proteins that should be biotinylated by TurboID. Ensure that the name of the column in the TP list matches the dataset.
+#' @param FP A list or dataframe containing the false positive proteins, used for flagging. Contains proteins that do not belong in the cellular compartment being analysed. Ensure that the name of the column in the FP list matches the dataset.
+#' @param gene_name The name of the column in data that contains the gene or protein names. Make sure that the name of the column in the TP and FP list matches the dataset
+#' @param ratio The name of the column in data that contains the ratio of interest
+#' @param TP_flag The name of the column to be created or modified in data for the true positive flag, so whether this protein is a TP for SYP or NES respectively
 #' @return The initial dataset, including TPR and FPR 
 #' @export
-flag <- function(data, sample, TP, FP, gene_name, ratio, TP_flag) {
+flag <- function(data, sample, TP, FP = "FP", gene_name = "gene_name", ratio, TP_flag) {
   #Flagging TP and FP proteins in the dataset
   data[[TP_flag]] <- ifelse(data[[gene_name]] %in% TP$gene_name, 1, NA)
   data$FP <- ifelse(data[[gene_name]] %in% FP$gene_name, 1, NA)
   #Calculating the median of the rations of the FP proteins
   subset <- data[!is.na(data$FP) & data$FP == 1, ]
-  median <- median(subset[[ratio]], na.rm = TRUE) ##please check the name of the ratio column, it needs to match the dataset
+  median <- median(subset[[ratio]], na.rm = TRUE) 
   #Summing TP and FP
   sumTP <- sum(data[[TP_flag]], na.rm = TRUE)
   sumFP <- sum(data$FP, na.rm = TRUE)
   #Normalising and sorting
-  col_name <- paste0(sample, "_FP")
+  col_name <- paste0(sample, "_FP_norm")
   data <- data %>%
     mutate(Sample = sample,
            !!sym(col_name) := .data[[ratio]] / median) %>% ##dynamic column naming
@@ -47,4 +48,64 @@ flag <- function(data, sample, TP, FP, gene_name, ratio, TP_flag) {
     }
   }
   return(data)
+}
+#' @title ROC Curve
+#' @description This function creates a ROC curve using the FPR and TPR columns in the dataset.
+#' @param data The dataset that will be used. Ensure that the columns are named appropriately (FPR and TPR).
+#' @param FPR The name of the column in data that contains the False Positive Rate (optional if named appropriately).
+#' @param TPR The name of the column in data that contains the True Positive Rate (optional if named appropriately).
+#' @param title The title of the ROC curve (optional).
+#' @param col1 The color of the points in the ROC curve (optional).
+#' @param col2 The color of the diagonal line in the ROC curve (optional).
+#' @return A ggplot2 object showing the ROC curve.
+#' @export
+roc <- function(data, FPR = "FPR", TPR = "TPR", title = "ROC Curve", col1 = "#002C34", col2 = "#4F0433") {
+  p <- ggplot(data = data, aes(x = FPR, y = TPR)) +
+    geom_point(color1, shape = 19, size = 1) +
+    geom_abline(intercept = 0, slope = 1, color2) +
+    xlab("FPR") +
+    ylab("TPR") +
+    labs(title = title) +
+    theme(plot.title = element_text(size = 20)) +
+    theme(plot.background = element_rect(fill = "white"),
+          panel.background = element_rect(fill="white"),
+          axis.line = element_line("black") )
+  return(p)
+}
+#' @title Graph to calculate the optimal Cutoff
+#' @description This function creates a graph to calculate the optimal cutoff for the dataset. The optimal cutoff is the point on the ROC curve that maximises the TPR-FPR value. The function calculates the log2 of the normalised ratio and the TPR-FPR value, and plots the graph.
+#' @param data The dataset that will be used. Ensure that the columns are named appropriately (FPR and TPR).
+#' @param sample The name of the sample (e.g. SYP or NES)
+#' @param col1 The color of the points in the graph (optional).
+#' @param col2 The color of the vertical line in the graph (optional).
+#' @param title The title of the graph (optional).
+#' @return A ggplot2 object showing the graph to calculate the optimal cutoff.
+#' @export
+opt_cutoff <- function(data, sample, col1 = "#002C34", col2 = "#4F0433", title = "Optimal cutoff") {
+  #Constructing column names
+  norm_col <- paste0(sample, "_FP")
+  log_col <- paste0("log2_", sample)
+  max_col <- paste0("max_", sample)
+  tpr_fpr <- "TPR-FPR"
+  if (!norm_col %in% names(data)) {
+    stop("Column '", norm_col, "' does not exist in the dataset. Please ensure the flag() function has been run, and that values have been normalised and TPR and FPR have been calculated.")
+  }
+  data <- data %>%
+    mutate(!!log_col := log2(as.numeric(.[[norm_col]])), 
+           !!tpr_fpr := TPR - FPR)
+  max_col <- max(data[[tpr_fpr]], na.rm = TRUE)
+  xintersect <- data[[log_col]][data[[tpr_fpr]] == max_col]
+  xintersect <- xintersect[1] #takes the first in case there are multiple. This might not be ideal and I might need to manually pick the peak instead.
+  y_position <- max(data[[tpr_fpr]], na.rm = TRUE) * 0.95 #arranges where the label will be on the graph
+  p <- ggplot(data, aes(x = !!sym(log_col), y = !!sym(tpr_fpr))) +
+    geom_point(color = col1, shape = 19, size = 0.5) + 
+    geom_vline(xintercept = xintersect, color = col2) +
+    annotate("text", x = xintersect, y = y_position, label = round(xintersect, 2), 
+             vjust = -0.5, color = col2, angle = 0, size = 4) +
+    xlab(log_col) +
+    ylab("TPR-FPR") +
+    labs(x = log_col, y = tpr_fpr, title = title) +
+    theme_minimal()
+  
+  return(p)
 }
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # TurboIDanalysis
-A package that contains functions to create the ROC curves from Cho et al., 2020 (Nat Protoc). It calculates TPR and FPR based on user-provided true- and false-positive lists, as well as the provided dataset with protein measurements. A previously calculated ratio of the protein measurements (sample/biotin control) is required.
+A package that contains functions to create the ROC curves following the data analysis in Cho et al., 2020 (Nat Protoc). It calculates the TPR and FPR based on user-provided true- and false-positive lists, as well as the provided dataset with protein measurements. A previously calculated ratio of the protein measurements (experimental sample/omit-ligase control) is required.
+
+## Features
+- Function that flags each protein as either a true or false positive, based on the user-provided lists, and calculates the True Positive and False Positive Rates.
+- Function that creates the ROC curve based on the TPR and FPR calculated by the previous function.
+- Function that plots the TPR-FPR against the log2 fold change of the protein measurements, allowing for the calculation of the optimal cutoff value.
+
+### Main functions
+- flag() - Flags each protein as either a true or false positive, based on the user-provided lists, and calculates the True Positive and False Positive Rates.
+- roc() - Creates the ROC curve based on the TPR and FPR calculated by the previous function.
+
+## Installation
+
+Install the package directly from GitHub using `devtools`:
+
+```R
+# Install devtools if not already installed
+install.packages("devtools")
+
+# Install package from GitHub
+devtools::install_github("chrtsa/TurboIDanalysis")
+```
+
+## Usage
+```R
+library(TurboIDanalysis)
+# Example: normalise the data and calculate the True Positive and False Positive Rates for all proteins detected in the experiment
+# Load the necessary datasets
+data <- "path/to/data.csv"
+TP_NES <- "path/to/TP_NES.csv"
+FP <- "path/to/FP.csv"
+# Create new columns with the TPR and FPR of each protein, arranged by descending order according to their levels of enrichment in the experimenta sample compared to the omitted-ligase control
+data <- flag(data = data, sample = "NES", TP = TP_NES, FP = FP, gene_name = "gene_name", ratio = "NES", TP_flag = "NES_TP")
+
+# Create the ROC curve for each potential cutoff value
+roc(data = data)
+
+# Plot the TPR-FPR against the log2 fold change of the protein measurements to determine the optimal cutoff value
+opt_cutoff(data = data)
+```
+
+### Reference
+Cho, K.F., Branon, T.C., Udeshi, N.D. et al. Proximity labeling in mammalian cells with TurboID and split-TurboID. Nat Protoc 15, 3971–3999 (2020). https://doi.org/10.1038/s41596-020-0399-0
